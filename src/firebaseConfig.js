764ace79759112db0ff6233e50c77eaef1feca96
BossLakryM <bosslakrym@gmail.com>
2023-10-30 10:20:38 +0100

    Amazon Clone

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCu8Avp75EK12wxmg29uqGq-Z4eFv6je2I",
+  authDomain: "clone-c9fa6.firebaseapp.com",
+  projectId: "clone-c9fa6",
+  storageBucket: "clone-c9fa6.appspot.com",
+  messagingSenderId: "390942056422",
+  appId: "1:390942056422:web:318405c70dfcabc1568027"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
